3402b7f5efbb6f5377346307843989e8f4e98345
SokHun <mlyhour57@gmail.com>
2026-04-05 16:59:26 +0700

    add feature sub

--- calculator.py
+++ calculator.py
@@ -16,8 +16,8 @@ def add(a, b):
     pass
 
 def subtract(a, b):
-    result = a - b 
-    log_action("subtract", result)
+    # result = a - b 
+    # log_action("subtract", result)
     # return result
     pass
 
